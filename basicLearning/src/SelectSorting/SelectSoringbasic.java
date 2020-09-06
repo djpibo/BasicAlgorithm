ec39d840a7a4fed42ec6d0307aa2490ea8c0b3b0
SG <djpibo@naver.com>
2020-09-06 22:18:45 +0900

    선택 정렬 알고리즘

--- basicLearning/src/SelectSorting/SelectSoringbasic.java
+++ basicLearning/src/SelectSorting/SelectSoringbasic.java
@@ -11,75 +11,83 @@ public class SelectSoringbasic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[] = {3,2,1,4,9,6,5,8,7};
-		for(int i =0; i<arr.length; i++)
-			System.out.print(arr[i] + " ");
-		System.out.println("");
 		boolean mode = true; // 기본은 오름차순(ASC)
-		sorting(arr,mode);	
 
-		System.out.println("\n");
+		SimpleClass sc = new SimpleClass();
+		sc.showArray(arr);
+		sc.sorting(arr,mode);	
+		sc.showArray(arr);
+	}
+}
+
+class SimpleClass{
+	
+	public void showArray(int[] arr) {
+		// TODO Auto-generated method stub
 		for(int i =0; i<arr.length; i++)
 			System.out.print(arr[i] + " ");
 	}
 
-	private static void sorting(int[] arr, boolean mode) {
+	public void sorting(int[] arr, boolean mode) {
 		// TODO Auto-generated method stub
 		int size = arr.length;
 		int index=0;
 		int temp;
 		if(mode) {
-			
+
+			// 1) 탐색
 			for(int j=0;j<size-1;j++) {
 				int min = 100;
 				for(int i=j; i<size;i++) {
 					if(min>arr[i]) {
-						 min = arr[i];
-						 index = i;
+						min = arr[i];
+						index = i;
 					}
 				}
+				// 2) 정렬
 				temp = arr[j];
 				arr[j] = min;
 				arr[index] = temp;
-				
+
 				System.out.println("\n");
 				for(int i =0; i<arr.length; i++)
 					System.out.print(arr[i] + " ");
 				System.out.print("\n"+(j)+"회차 min: "+arr[j] + " index: "+index);
-				
+
 				System.out.println();
 				for(int i =0; i<arr.length; i++)
 					System.out.print(arr[i] + " ");
-				
+
 			}
-			
+
 		}
 		else {
-			
+
 			for(int j=0;j<size-1;j++) {
 				int min = -100;
 				for(int i=j; i<size;i++) {
 					if(min<arr[i]) {
-						 min = arr[i];
-						 index = i;
+						min = arr[i];
+						index = i;
 					}
 				}
 				temp = arr[j];
 				arr[j] = min;
 				arr[index] = temp;
-				
+
 				System.out.println("\n");
 				for(int i =0; i<arr.length; i++)
 					System.out.print(arr[i] + " ");
 				System.out.print("\n"+(j)+"회차 min: "+arr[j] + " index: "+index);
-				
+
 				System.out.println();
 				for(int i =0; i<arr.length; i++)
 					System.out.print(arr[i] + " ");
-				
+
 			}
-			
+
 		}
-		
+
 	}
 
 }
